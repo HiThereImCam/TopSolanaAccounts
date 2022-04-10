0556d8718a75a742d07338421f81c3e60bd559db
Cameron Tanjoco <ctanjoco@gmail.com>
2022-04-09 22:25:36 -0700

    created new server/frontend folders. the tsconfig at root compiled frontend code to .js files and the work around was creating different folders to separate the two. added cors library and stringified solana accounts. frontend displays solana accounts.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useReducer } from "react";
+import "./App.css";
+
+import { getSolanaAccounts, AccountInfo } from "./util/getSolanaAccounts";
+
+type reducerState = {
+  loading: boolean;
+  error: string;
+};
 
 function App() {
+  const [accountData, setAccountData] = useState<Array<AccountInfo>>([]);
+  const [state, setState] = useReducer(
+    (state: reducerState, newState: reducerState) => ({
+      ...state,
+      ...newState,
+    }),
+    { loading: true, error: "" }
+  );
+
+  let getAccounts = async () => {
+    let solanaAccountsString: any = await fetch(
+      "http://localhost:8080/solana/accounts"
+    );
+    let solanaAccounts = await solanaAccountsString.json();
+    if (solanaAccounts) {
+      setAccountData(solanaAccounts);
+      setState({ loading: false, error: "" });
+    }
+  };
+
+  useEffect(() => {
+    getAccounts();
+  }, []);
+
+  // let { loading, error } = state;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!!accountData && accountData.length > 0 ? (
+        <div>
+          <pre>{JSON.stringify(accountData, null, 2)}</pre>
+        </div>
+      ) : null}
     </div>
   );
 }
